44df196b694aa7466246781f55aa7584a1ba34b6
fabrarry <fabrarry@gmail.com>
2024-06-14 19:53:58 +0900

    ♻️ 不要コード削除

--- src/app/api/graphql/countries/route.ts
+++ src/app/api/graphql/countries/route.ts
@@ -28,12 +28,9 @@ const GET_COUNTRIES = gql`
 `;
 
 export async function GET() {
-  console.log("Fetching countries from network");
   const { data } = await client.query({
     query: GET_COUNTRIES,
     fetchPolicy: "no-cache", // Always fetch from network
   });
-
-  console.log("test GET countries data: ", data);
   return NextResponse.json(data);
 }
